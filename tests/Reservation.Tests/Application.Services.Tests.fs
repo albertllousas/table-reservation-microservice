25df793cee76dc772c30ea20172a5811fa943073
Albert Llousas <albert.llousas@n26.com>
2022-08-03 13:05:19 +0200

    Add missing test

--- tests/Reservation.Tests/Application.Services.Tests.fs
+++ tests/Reservation.Tests/Application.Services.Tests.fs
@@ -15,6 +15,8 @@ let private when' = DateTime.UtcNow
 
 let private table: Table = { TableId = tableId; RestaurantId = Guid.NewGuid(); Capacity = 4; Date = DateOnly.FromDateTime(when') }
 
+let private reservationRequest: InputPorts.ReserveTableRequest = { TableId = tableId.Value; When = when'; Persons = 3; Name = "John Doe" }
+
 let private idGenerator = {
     new OutputPorts.IdGenerator with 
       member _.Hash() = "hash"
@@ -32,7 +34,7 @@ let tests =
       let tableRepositoryMock= Mock<OutputPorts.TableRepository>().Setup(fun mock -> <@ mock.FindBy tableId @>).Returns(Ok table).Create()
       let reserveTable: InputPorts.ReserveTable = Services.reserveTable tableRepositoryMock idGenerator reserveSuccesfully
 
-      let result = reserveTable { TableId = tableId.Value; When = when'; Persons = 3; Name = "John Doe" }
+      let result = reserveTable reservationRequest
 
       let expectedResult: InputPorts.ReserveTableResponse = { TableId = tableId.Value; ReservationRef = "hash" }
       Assert.Equal(result, Ok expectedResult)
@@ -43,9 +45,20 @@ let tests =
       let tableRepositoryMock= Mock<OutputPorts.TableRepository>().Setup(fun mock -> <@ mock.FindBy tableId @>).Returns(Error TableNotFound).Create()
       let reserveTable: InputPorts.ReserveTable = Services.reserveTable tableRepositoryMock idGenerator reserveSuccesfully
 
-      let result = reserveTable { TableId = tableId.Value; When = when'; Persons = 3; Name = "John Doe" }
+      let result = reserveTable reservationRequest
 
       Assert.Equal(result, Error TableNotFound)
+      Mock.Verify(<@ tableRepositoryMock.Save table @>, never)
+    }
+
+    test "Should fail orchestrating the reservation when reserve itself fails" {
+      let tableRepositoryMock= Mock<OutputPorts.TableRepository>().Setup(fun mock -> <@ mock.FindBy tableId @>).Returns(Ok table).Create()
+      let reserveFails : Table.Reserve = fun _ _ -> Error TableAlreadyReserved
+      let reserveTable: InputPorts.ReserveTable = Services.reserveTable tableRepositoryMock idGenerator reserveFails
+
+      let result = reserveTable reservationRequest
+
+      Assert.Equal(result, Error TableAlreadyReserved)
       Mock.Verify(<@ tableRepositoryMock.Save table @>, never)
     }
   ]
